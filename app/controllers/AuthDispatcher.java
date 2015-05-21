34fb75b27a3b013262f6702d9ea41873b33684a5
Putis <marek.28.93@gmail.com>
2015-05-21 09:49:19 +0200

    dodałem ścieżki do logowania/rejestracji

---
+++ app/controllers/AuthDispatcher.java
@@ -0,0 +1,26 @@
+package controllers;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import play.libs.ws.WS;
+import play.libs.ws.WSResponse;
+import play.mvc.Controller;
+import play.mvc.Result;
+
+/**
+ * Created by Marek on 2015-05-21.
+ *
+ * Dispatches requests to authentication service
+ */
+public class AuthDispatcher extends Controller {
+    private static final long TIMEOUT = 5000;
+
+    public static Result dispatchRequest(String path) {
+        JsonNode jsonNode = request().body().asJson();
+
+        WSResponse wsResponse = WS.url("http://partyadvisor.herokuapp.com" + path)
+                .post(jsonNode)
+                .get(TIMEOUT);
+
+        return ok(wsResponse.asJson());
+    }
+}
