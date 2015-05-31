e6eba49c8195a07df035df4d8f18ea2cec810fd9
Putis <marek.28.93@gmail.com>
2015-06-01 00:06:43 +0200

    poprawiono kolejność argumentów

--- test/controllers/ProfileServiceConnectorTest.java
+++ test/controllers/ProfileServiceConnectorTest.java
@@ -7,8 +7,6 @@ import play.test.FakeRequest;
 
 import static org.junit.Assert.assertEquals;
 import static play.test.Helpers.*;
-import static play.test.Helpers.contentAsString;
-import static play.test.Helpers.status;
 
 public class ProfileServiceConnectorTest {
 
@@ -21,24 +19,21 @@ public class ProfileServiceConnectorTest {
 
     @Test
     public void testGetProfile() throws Exception {
-        running(fakeApplication(), new Runnable() {
-            @Override
-            public void run() {
-                // this token is for bussiness user: krzysiek@gmail.com pass: krzysiek@gmail.com
-                String token = "eyJjdHkiOiJ0ZXh0XC9wbGFpbiIsImFsZyI6IkhTMjU2In0.eyJ1c2VySWQiOiI1NTU2MTc0YmU0YjAxY2EyNDViMjdlZDIiLCJ1c2VyUm9sZSI6ImJ1c2luZXNzIn0.DYFNXv7e8zwTOhi9eEV5apb7IZafiryuDf_wlYvS1ec";
-                String idforThisToken = "5556174be4b01ca245b27ed2";
-                //
-                String tokenKawiarni = "eyJjdHkiOiJ0ZXh0XC9wbGFpbiIsImFsZyI6IkhTMjU2In0.eyJ1c2VySWQiOiI1NTRmYzg1OWU0YjBkYjFhZTc1MGI0M2YiLCJ1c2VyUm9sZSI6ImJ1c2luZXNzIn0.Xo3xCNWA38T-mVGaRISzRdydpzlo8ApvnO4FDwmMuMI";
-                String idKawiarni = "554fc859e4b0db1ae750b43f";
-                FakeRequest request = fakeRequest("GET", "/profiles/business").withHeader("Authorization","Bearer "+tokenKawiarni);
-                Result result = route(request);
+        running(fakeApplication(), () -> {
+            // this token is for bussiness user: krzysiek@gmail.com pass: krzysiek@gmail.com
+            String token = "eyJjdHkiOiJ0ZXh0XC9wbGFpbiIsImFsZyI6IkhTMjU2In0.eyJ1c2VySWQiOiI1NTU2MTc0YmU0YjAxY2EyNDViMjdlZDIiLCJ1c2VyUm9sZSI6ImJ1c2luZXNzIn0.DYFNXv7e8zwTOhi9eEV5apb7IZafiryuDf_wlYvS1ec";
+            String idforThisToken = "5556174be4b01ca245b27ed2";
+            //
+            String tokenKawiarni = "eyJjdHkiOiJ0ZXh0XC9wbGFpbiIsImFsZyI6IkhTMjU2In0.eyJ1c2VySWQiOiI1NTRmYzg1OWU0YjBkYjFhZTc1MGI0M2YiLCJ1c2VyUm9sZSI6ImJ1c2luZXNzIn0.Xo3xCNWA38T-mVGaRISzRdydpzlo8ApvnO4FDwmMuMI";
+            String idKawiarni = "554fc859e4b0db1ae750b43f";
+            FakeRequest request = fakeRequest("GET", "/profiles/business").withHeader("Authorization","Bearer "+tokenKawiarni);
+            Result result = route(request);
 
-                assertEquals("When correct token in get request: "+ contentAsString(result),
-                        200, status(result));
-                assertEquals(contentAsString(result), "{\"_id\":{\"$oid\":\"554fc859e4b0db1ae750b43f\"},\"name\":\"kawiarnia\",\"category_list\":null,\"about\":null,\"location\":{\"city\":null,\"country\":null,\"street\":null},\"locationCoordinates\":{\"xCoordinate\":null,\"yCoordinate\":null},\"website\":null,\"music_genres\":null,\"phone\":null,\"picture_url\":null}");
-                System.out.println(contentAsString(result));
+            assertEquals("When correct token in get request: "+ contentAsString(result),
+                    200, status(result));
+            assertEquals("{\"_id\":{\"$oid\":\"554fc859e4b0db1ae750b43f\"},\"name\":\"kawiarnia\",\"category_list\":null,\"about\":null,\"location\":{\"city\":null,\"country\":null,\"street\":null},\"locationCoordinates\":{\"xCoordinate\":null,\"yCoordinate\":null},\"website\":null,\"music_genres\":null,\"phone\":null,\"picture_url\":null}", contentAsString(result));
+            System.out.println(contentAsString(result));
 
-            }
         });
     }
 }
